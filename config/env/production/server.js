e4a13b371ae5d905009c01290081d13269f72307
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-08-03 22:09:32 -0300

    Update production config files

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,8 @@
+// path: ./config/env/production/server.js
+
 module.exports = ({ env }) => ({
-  url: env("APP_URL"),
   proxy: true,
+  url: env("APP_URL"), // replaces `host` and `port` properties in the development environment
   app: {
     keys: env.array("APP_KEYS"),
   },
